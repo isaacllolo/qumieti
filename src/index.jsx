0ec4d07c64c618ae2669feca944bab650c308d97
Isaac David Llorente Lopez <82958519+isaacllolo@users.noreply.github.com>
2024-01-26 13:35:09 -0500

    try vite 4 rsilway

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
